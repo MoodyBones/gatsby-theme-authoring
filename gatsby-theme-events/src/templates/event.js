6da9790d76bd6d5546889855f3a5703adb3f3ece
Mel <jones.mel@gmail.com>
2019-08-25 14:38:47 +0200

    display sorted Data

---
+++ gatsby-theme-events/src/templates/event.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { graphql } from 'gatsby'
+
+// the reason we export a page query is because we need access to graphql
+export const 
+
+const EventTemplate = () => <p>TODO build the event page</p>
+
+export default EventTemplate
