4fc63dd64aa193be566cc785baa8e7bbc2431467
Mel <jones.mel@gmail.com>
2019-08-26 14:09:36 +0200

    clean up, trying to fix graphql error

--- gatsby-theme-events/.cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---src-templates-events-js": () => import("/Users/mel/Documents/web/lets-play/gatsby/gatsby-theme-authoring/gatsby-theme-events/src/templates/events.js" /* webpackChunkName: "component---src-templates-events-js" */),
-  "component---src-templates-event-js": () => import("/Users/mel/Documents/web/lets-play/gatsby/gatsby-theme-authoring/gatsby-theme-events/src/templates/event.js" /* webpackChunkName: "component---src-templates-event-js" */),
-  "component---cache-dev-404-page-js": () => import("/Users/mel/Documents/web/lets-play/gatsby/gatsby-theme-authoring/gatsby-theme-events/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
-}
-
